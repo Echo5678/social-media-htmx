5ac2ffecc5d4bacb9ce6651044212d3094ae3000
echo <echoevader1@gmail.com>
2023-09-12 16:22:57 -0700

    Cleaned up index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,9 @@
 import { Elysia, t, ws } from "elysia";
 import { html } from "@elysiajs/html";
 import { swagger } from "@elysiajs/swagger";
-import { cookie } from "@elysiajs/cookie";
-import { jwt } from "@elysiajs/jwt";
-
-import { db } from "./db/client";
-import { users } from "./db/schema";
-import { sql } from "drizzle-orm";
 
 import { BaseHtml } from "./pages/basehtml";
 import { auth, home } from "./routes";
-
-const WEEK = 60 * 60 * 24 * 7;
 
 const app = new Elysia()
   .use(
@@ -24,47 +16,8 @@ const app = new Elysia()
       },
     })
   )
-  .use(
-    jwt({
-      name: "jwt",
-      secret: process.env.JWT_SECRET as string,
-    })
-  )
-  .use(
-    cookie({
-      httpOnly: true,
-      maxAge: WEEK,
-      sameSite: "strict",
-      signed: true,
-      secret: process.env.COOKIE_SECRET as string,
-    })
-  )
   .use(html())
   .use(ws())
-  .derive(async ({ jwt, cookie: { user } }) => {
-    let userAuthorized;
-
-    if (!user) {
-      return userAuthorized;
-    }
-
-    const userJWT: any = await jwt.verify(user);
-    const User: any = await db
-      .select({
-        username: users.username,
-        email: users.email,
-      })
-      .from(users)
-      .where(sql`${users.email} = ${userJWT.email} and ${user} = ${users.jwt}`)
-      .limit(1);
-
-    if (User) {
-      userAuthorized = User[0];
-    }
-    return {
-      userAuthorized,
-    };
-  })
   .use(auth)
   .use(home)
   .ws("/message", {
