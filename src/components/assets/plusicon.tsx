121edc60afba9ce3f59600813b819f4bbfecf582
echo <echoevader1@gmail.com>
2023-09-15 16:45:10 -0700

    Sidebar Navbar

---
+++ src/components/assets/plusicon.tsx
@@ -0,0 +1,16 @@
+export default function PlusIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      class={className}
+    >
+      <path
+        fill="currentColor"
+        d="M8.5 2.75a.75.75 0 0 0-1.5 0V7H2.75a.75.75 0 0 0 0 1.5H7v4.25a.75.75 0 0 0 1.5 0V8.5h4.25a.75.75 0 0 0 0-1.5H8.5V2.75Z"
+      />
+    </svg>
+  );
+}
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,45 +1,12 @@
-import FriendsList from "../components/friendslist";
 import Navbar from "../components/navbar";
-import Post from "../components/post";
-import Trending from "../components/trending";
-import fakePosts from "../posts.json";
+import SideBar from "../components/sidebar";
 
 export default function HomePage() {
   return (
-    <div
-      id="homepage-container"
-      class="flex flex-col h-[100vh] md:flex-row overflow-y-hidden"
-    >
-      <div
-        id="homepage-left"
-        class="flex justify-center items-center invisible md:visible md:w-1/4 h-screen relative"
-      >
-        <FriendsList />
-      </div>
-      <div
-        id="homepage-main"
-        class="w-[100%] flex flex-col md:w-2/4 h-screen overflow-y-scroll top-0"
-      >
-        <div class="md:border-x-[1px]">
-          {fakePosts.map((item) => (
-            <Post
-              creator={item.creator}
-              handle={item.handle}
-              profilePic={item.profilePic}
-              timestamp={item.timestamp}
-              title={item.title}
-              technologies={item.technologies}
-            />
-          ))}
-          <Navbar />
-        </div>
-      </div>
-      <div
-        id="homepage-right"
-        class="invisible md:visible md:w-1/4 md:h-[100%] flex"
-      >
-        <Trending />
-      </div>
-    </div>
+    <main class="flex">
+      <SideBar />
+      <p>Content on the page.</p>
+      <Navbar />
+    </main>
   );
 }
