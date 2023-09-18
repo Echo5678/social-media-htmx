b99ad785821a8652cc258948a819bf5ef0537111
elijah-mccoy5 <63271126+elijah-mccoy5@users.noreply.github.com>
2023-09-18 01:14:29 -0700

    feat: star component replacing upvotes/downvotes

---
+++ src/routes/user/index.tsx
@@ -0,0 +1,67 @@
+import { Elysia, t } from "elysia";
+import html from "@elysiajs/html";
+import cookie from "@elysiajs/cookie";
+import jwt from "@elysiajs/jwt";
+
+import { db } from "../../db/client";
+import { users } from "../../db/schema";
+import { sql } from "drizzle-orm";
+
+const WEEK = 60 * 60 * 24 * 7;
+
+export const auth = (app: Elysia) =>
+  app
+    .use(html())
+    .use(
+      jwt({
+        name: "jwt",
+        secret: process.env.JWT_SECRET as string,
+      })
+    )
+    .use(
+      cookie({
+        httpOnly: true,
+        maxAge: WEEK,
+        sameSite: "strict",
+        signed: true,
+        secret: process.env.COOKIE_SECRET as string,
+      })
+    )
+    .derive(async ({ jwt, cookie: { user } }) => {
+      let userAuthorized;
+
+      if (!user) {
+        return userAuthorized;
+      }
+
+      const userJWT: any = await jwt.verify(user);
+
+      if (!userJWT) {
+        return userAuthorized;
+      }
+
+      const User: any = await db
+        .select({
+          username: users.username,
+          email: users.email,
+        })
+        .from(users)
+        .where(
+          sql`${users.email} = ${userJWT.email} and ${user} = ${users.jwt}`
+        )
+        .limit(1);
+
+      if (User) {
+        userAuthorized = User[0];
+      }
+      return {
+        userAuthorized,
+      };
+    })
+    .get("/badges/:id", async ({ params: { id }, userAuthorized, set }) => {
+      const user = userAuthorized;
+      if (!user) {
+        set.status = 307;
+        set.redirect = "/sign-in";
+      }
+    });
