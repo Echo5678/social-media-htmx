206828e8a544d5e4b91d7858b10488215fca415d
echo <echoevader1@gmail.com>
2023-11-11 01:42:46 -0800

    bug: redis stuff

--- src/db/redis-client.ts
+++ src/db/redis-client.ts
@@ -2,6 +2,10 @@ import { createClient } from "redis";
 
 const client = createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    port: Number(process.env.REDIS_PORT),
+    host: process.env.REDIS_HOST,
+  },
 });
 
 client.on("error", (err: any) =>
