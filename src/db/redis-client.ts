8d37f6f4ed253915362601c2ff18bcd53094fef0
echo <echoevader1@gmail.com>
2023-11-11 11:37:41 -0800

    bug: already know this won't work :(

--- src/db/redis-client.ts
+++ src/db/redis-client.ts
@@ -2,6 +2,10 @@ import { createClient } from "redis";
 
 const client = createClient({
   url: process.env.REDIS_URL,
+  socket: {
+    port: process.env.REDIS_PORT as number,
+    host: process.env.REDIS_HOST,
+  },
 });
 
 client.on("error", (err: any) =>
