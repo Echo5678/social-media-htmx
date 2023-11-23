90bef2e7311c5fb6e5093afb6556ecd00cde8a6d
echo <vincentgarciajr2712@gmail.com>
2023-11-22 23:42:48 -0800

    bug: fixed project list link undefined error

--- src/components/projectlist.tsx
+++ src/components/projectlist.tsx
@@ -22,7 +22,7 @@ export default function ProjectList({
       {projects.map((item) => (
         <li class="bg-[#f9f9f9] dark:bg-[#010101] border border-zinc-300 dark:border-zinc-800 rounded-lg w-full hover:cursor-pointer flex  flex-col p-5">
           <a
-            href={`/project/${item.id}`}
+            href={`/project/${item?.project_id}`}
             hx-boost="true"
             hx-push-url
             class="flex flex-col flex-grow"
